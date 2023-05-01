a0c9264d598f63666d53920eea87bd7ebdd7dde0
shaun-moate <shaun.moate@gmail.com>
2023-05-01 18:01:33 +0100

    Resume.Init: initial commit

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Shaun.",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Online resume of Shaun Moate, inspired by https://andris.gauracs.com/",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,8 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/shauntmoate",
+    github: "https://github.com/shaun-moate",
+    docs: "https://github.com/shaun-moate/online-resume",
   },
 }

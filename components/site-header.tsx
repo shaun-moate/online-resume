8e2d44b729bc41cd49134e3923d543e78db937df
shaun-moate <shaun.moate@gmail.com>
2023-05-07 19:31:37 +0100

    About.SVG: added svg and animated it

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,7 +4,6 @@ import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
-import { ThemeToggle } from "@/components/theme-toggle"
 
 export function SiteHeader() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -65,10 +65,46 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "wiggle": {
+          '0%, 100%': { transform: 'rotateZ(-1deg)' },
+          '50%': { transform: 'rotateZ(1deg)' },
+        },
+        "white-grey": {
+          '0%, 100%': { fill: 'white' },
+          '50%': { fill: '#E6E6E6' },
+        },
+        "white-blue": {
+          '0%, 100%': { fill: 'white' },
+          '50%': { fill: '#6C63FF' },
+        },
+        "blue-grey": {
+          '0%, 100%': { fill: '#6C63FF' },
+          '50%': { fill: '#E6E6E6' },
+        },
+        "blue-white": {
+          '0%, 100%': { fill: '#6C63FF' },
+          '50%': { fill: 'white' },
+        },
+        "grey-white": {
+          '0%, 100%': { fill: '#E6E6E6' },
+          '50%': { fill: 'white' },
+        },
+        "grey-blue": {
+          '0%, 100%': { fill: '#E6E6E6' },
+          '50%': { fill: '#6C63FF' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "wiggle": "wiggle 1s ease-in-out infinite",
+        "wiggle-fast": "wiggle 200ms ease-in-out infinite",
+        "white-grey": "white-grey 1800ms infinite",
+        "white-blue": "white-blue 2000ms infinite",
+        "blue-grey": "blue-grey 2200ms infinite",
+        "blue-white": "blue-white 2100ms infinite",
+        "grey-white": "grey-white 1900ms infinite",
+        "grey-blue": "grey-blue 2000ms infinite",
       },
     },
   },

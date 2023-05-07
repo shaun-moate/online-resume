49e267455a1f7a00a6c6e9958912cf3c80a200ac
shaun-moate <shaun.moate@gmail.com>
2023-05-07 22:33:45 +0100

    About.Desc: added terminal-like method

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,27 @@
 'use client'
 
-import Image from "next/image"
 import Typewriter from "typewriter-effect"
 
-import intro from "/public/intro.png"
+import { DeveloperSVG } from "@/components/assets/developer-svg"
+import { Terminal } from "@/components/terminal"
 
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <div className="flex max-w-[980px] flex-row items-start gap-2">
-          <Image className="pt-36 pl-20"
-            src={intro}
-            alt="my intro image"
-            height={640}
-            width={640}
-          />
-        </div>
-        <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <h1 className="pt-60 text-2xl text-red-600 font-bold leading-tight tracking-tighter sm:text-2xl md:text-3xl lg:text-5xl">
-            I am
-          </h1>
-          <h1 className="pb-80 text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            <Typewriter
-              options={{
-                strings: ['a data engineer', 'an avid technologist', 'an engineering leader'],
-                autoStart: true,
-                loop: true,
-              }}
-            />
-          </h1>
-        </div>
+      <div className="grid grid-cols-2 pt-32">
+        <DeveloperSVG />
+        <Terminal />
+      </div>
+      <div>
+      <h1 className="pt-60 text-2xl font-extrabold leading-tight tracking-tighter sm:text-2xl md:text-3xl lg:text-4xl">
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString('experience<strong style="color:blue;">.</strong>')
+              .pauseFor(2500)
+              .start();
+          }}
+        />
+      </h1>
       </div>
     </section>
   )

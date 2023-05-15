0e51b196b1785be99112694cce25e51eddb3b499
shaun-moate <shaun.moate@gmail.com>
2023-05-15 22:09:30 +0100

    Bin.Neofetch: removed icons set

--- lib/bin/neofetch.ts
+++ lib/bin/neofetch.ts
@@ -2,21 +2,21 @@ import { siteConfig } from "@/config/site";
 
 export const neofetch = () => {
     return `
-             @@@@@@@@@@@@@                    neofetch: summary of my details
+             @@@@@@@@@@@@@                  ** neofetch: summary of my details
         @@@@               @@@@             -----------
-      @@                       @@            ABOUT
-    @@                           @@          ${siteConfig.fullname}
-  @@                               @@        ${siteConfig.location}
- @@                         @@@     @@      ﰩ ${siteConfig.description}
-@@        @@@                        @@      <u><a href="${siteConfig.links.linkedin}" target="_blank">LinkedIn</a></u>
-@@                                   @@     爵<u><a href="${siteConfig.links.github}" target="_blank">Github</a></u>
-@@             .@@@@@@@@@@.          @@     -----------
- @@           @@          @@        @@       CONTACT
-  @@           @@        @@        @@        <u><a href="mailto:${siteConfig.email}" target="_blank">${siteConfig.email}</a></u>
-   @@             @@@@@@          @@         <u><a href="${siteConfig.links.linkedin}" target="_blank">linkedin.com/in/${siteConfig.links.user}</a></u>
+      @@                       @@           ** ABOUT
+    @@                           @@         * ${siteConfig.fullname}
+  @@                               @@       * ${siteConfig.location}
+ @@                         @@@     @@      * ${siteConfig.description}
+@@        @@@                        @@     * <u><a href="${siteConfig.links.linkedin}" target="_blank">LinkedIn</a></u>
+@@                                   @@     * <u><a href="${siteConfig.links.github}" target="_blank">Github</a></u>
+@@                                   @@     -----------
+ @@                                 @@      ** CONTACT
+  @@           @@        @@        @@       * <u><a href="mailto:${siteConfig.email}" target="_blank">${siteConfig.email}</a></u>
+   @@             @@@@@@          @@        * <u><a href="${siteConfig.links.linkedin}" target="_blank">linkedin.com/in/${siteConfig.links.user}</a></u>
      @@@                        @@@         -----------
-        @@@                  @@@ @@          PROJECTS
-         @|  @@@@@@@@@@@@@@@@   @@          tbd
+        @@@                  @@@ @@         ** PROJECTS
+         @|  @@@@@@@@@@@@@@@@   @@          * tbd
          @|                      @@
 `;
 };

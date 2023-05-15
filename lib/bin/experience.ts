4e02d91e869d77626fd81a25e98ca536e2231f91
shaun-moate <shaun.moate@gmail.com>
2023-05-15 11:04:31 +0100

    Bin.Experience: added resume as git log --graph

---
+++ lib/bin/experience.ts
@@ -0,0 +1,58 @@
+export const experience = () => {
+  return `
+    *   85a4880 (HEAD, master) 'Depop: enable our application engineering teams to deliver value to users faster and more reliably'
+    |\\
+    | :
+    | * 6057a0a (promoted) 2022-09: VP, Platform Engineering @ Depop
+    | :
+    | * c5130d2 (acquisition) 2021-05: Etsy, acquired Depop for $1.6BN
+    | * aa2d7e7 (new) 2021-05: Director of Engineering, Data @ Depop
+    |/
+    * a6f44d9 Merge branch 'DAZN: establishing and growing global data science function(s) to support hyper-growth start-up'
+    |\\
+    | * 5ddfrg3 (resigned) 2021-05: looking for next challenge after 4yrs with DAZN
+    | :
+    | :
+    | * 97612ed (promoted) 2019-09: VP, Applied ML and Analytics @ DAZN
+    | :
+    | * f3e66fc (promoted) 2018-07: Director of Data Science and Insights @ DAZN
+    | :
+    | * f3e66fc (new) 2017-07: Head of Insights @ DAZN
+    |/
+    * 02bfff3 Merge branch 'Aviva UK Digital: marturing use of data science in the insurance industry'
+    |\\
+    | * 5d539a3 (resigned) 2017-07: opportunity to join sports start-up (to hard to turn down)
+    | :
+    | * 50a334e (new) 2017-02: Lead Data Scientist @ Aviva
+    |/
+    * d185b26 Merge branch 'Sainsburys: re-thinking loyalty schemes for your large-scale grocers'
+    |\\
+    | * 3f9cdd9 (redundancy) 2016-12: investment in area dropped
+    | :
+    | * 6c044c8 (new) 2016-08: Data Science & Analytics Lead @ Sainsburys
+    |/
+    * bcc9b74 Merge branch 'Virgin Media: support financial modelling (churn prediction, forecasting)'
+    |\\
+    | * 5d539a3 (resigned) 2016-08: wanted move back to managing people & London (was based in Hook)
+    | :
+    | * 56ef96c (new) 2015-11: Data Scientist
+    |/
+    * 84b1d36 Merge branch 'Sky: supporting trading analysis and financial budgeting (churn prediction, forecasting)'
+    |\\
+    | * b5f5ef6 (redundancy) 2015-11: took voluntary redundancy in a restructure
+    | :
+    | * 691a8f6 (promoted) 2014-10: Startegy and Insights Controller @ Sky
+    | :
+    | * 50dc2ae (new) 2012-11: Trading Controller (Retail) @ Sky
+    |/
+    :
+    * 4dtgs2s (travel): 7 months travelling from Bangokok to London via land only (bus, train, and motorbike)
+    :
+    :
+    * 953sdtf (honours): TCA Indigenous Scholar, Queens College, University of Melbourne, Australia
+    * 989fe62 (education): Bachelor of Commerce, University of Melbourne, Australia
+    :
+    * f7d3186 initial commit
+    `;
+}
+
